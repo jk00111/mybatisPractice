9a3f282bbf90f3deab2a8ae282ad130be0ce2d1a
jkkim <jkkim@interfaceit.co.kr>
2024-07-01 16:33:00 +0900

    [수정]

--- tr_spring/src/main/java/com/kos/tr/news/login/controller/LoginController.java
+++ tr_spring/src/main/java/com/kos/tr/news/login/controller/LoginController.java
@@ -4,16 +4,18 @@ import com.kos.tr.news.login.dto.LoginDTO;
 import com.kos.tr.news.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService service;
 
-    @GetMapping
+    @PostMapping("/check")
     public ResponseEntity<Boolean> loginCheck(LoginDTO dto){
         return ResponseEntity.ok(service.loginCheck(dto));
     }
